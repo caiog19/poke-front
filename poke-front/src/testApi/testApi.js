08248de9cebc3c51c927b9f3dacbfbbbbef47ccc
Caio Gullo <caioinacio19@gmail.com>
2025-04-04 20:07:00 -0300

    feat: improve testapi

--- poke-front/src/testApi/testApi.js
+++ poke-front/src/testApi/testApi.js
@@ -8,17 +8,25 @@ import {
   
   async function testPokeApi() {
     try {
-      const list = await fetchPokemonList(0, 14);
+      const list = await fetchPokemonList(0, 10);
       console.log('Lista de Pokemon:', list?.results);
+      list?.results.forEach(pokemon => console.log(`- ${pokemon.name}`));
   
       const details = await fetchPokemonDetails(1);
-      console.log('Detalhes do Pokemon:', details);
+      console.log('Detalhes do Pokemon:');
+      console.log('Nome:', details.name);
+      console.log('Tipos:', details.types.map(t => t.type.name).join(', '));
+      console.log('Sprite:', details.sprites.front_default);
+      console.log('Ataques(5):', details.moves.slice(0, 5).map(m => m.move.name));
   
       const species = await fetchPokemonSpecies(1);
       console.log('Espécie do Pokemon:', species);
+      console.log('URL da evolução:', species.evolution_chain.url);
   
       const evolution = await fetchEvolutionChain(1);
       console.log('Cadeia Evolutiva:', evolution);
+      const evolutionChain = parseEvolutionChain(evolution.chain);
+      console.log('Evolução:', evolutionChain.join(' -> '));
   
       const fireType = await fetchPokemonByType('fire');
       console.log('Pokemons de Fogo: ', fireType?.pokemon.map(p => p.pokemon.name));
@@ -27,5 +35,15 @@ import {
     }
   }
   
+  function parseEvolutionChain(chain) {
+    const result = [];
+    let current = chain;
+    while (current) {
+      result.push(current.species.name);
+      current = current.evolves_to[0]; 
+    }
+    return result;
+  }
+
   testPokeApi();
   
