4fba2e87f7f72837f8bc12d050fb3bc00f8921e5
Caio Gullo <caioinacio19@gmail.com>
2025-04-05 10:17:13 -0300

    feat: implementing infinite scroll in home

--- poke-front/src/views/HomeView/HomeView.vue
+++ poke-front/src/views/HomeView/HomeView.vue
@@ -4,11 +4,10 @@
         <h1 class="title">Pokédex</h1>
 
         <div class="pokemon-list">
-            <PokemonCard 
-                v-for="pokemon in pokemonList"
-                :key="pokemon.name"
-                :name="pokemon.name"
-            />
+            <PokemonCard v-for="pokemon in pokemonList" :key="pokemon.name" :name="pokemon.name" />
+        </div>
+        <div v-if="isLoading" class="loading">
+            Carregando mais Pokémon...
         </div>
     </div>
 </template>
@@ -27,16 +26,45 @@ export default {
         return {
             pokemonList: [],
             totalCount: 0,
+            offset: 0,
+            limit: 20,
+            isLoading: false,
         };
     },
 
-    async mounted() {
-        const data = await fetchPokemonList(0, 20);
-        if (data && data.results) {
-            this.pokemonList = data.results;
-            this.totalCount = data.count;
+    mounted() {
+        this.loadMorePokemons();
+        window.addEventListener('scroll', this.handleScroll);
+    },
+    beforeUnmount(){
+        window.removeEventListener('scroll', this.handleScroll);
+    },
+
+    methods: {
+        async loadMorePokemons() {
+            if(this.isLoading) return;
+
+            this.isLoading = true;
+            const data = await fetchPokemonList(this.offset, this.limit);
+            if(data && data.results){
+                this.pokemonList.push(...data.results);
+                this.totalCount = data.count;
+                this.offset += this.limit;
+
+
+            }
+            this.isLoading =false;
+        },
+
+        handleScroll() {
+            const bottomReached = window.innerHeight + window.scrollY >= document.body.offsetHeight -10;
+
+            if (bottomReached && this.pokemonList.length <this.totalCount){
+                this.loadMorePokemons();
+            }
+
         }
-    }
+    },
 
 
 };
