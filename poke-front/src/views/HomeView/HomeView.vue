5ff6df4c4dccce8223d5bc3f150695b090da5c79
Caio Gullo <caioinacio19@gmail.com>
2025-04-04 21:01:51 -0300

    feat: implementing basic home and router

---
+++ poke-front/src/views/HomeView/HomeView.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="home-container">
+
+        <h1 class="title">Pokédex</h1>
+
+        <div class="pokemon-list">
+            <div class="pokemon-item" v-for="pokemon in pokemonList" :key="pokemon.name">
+                {{ pokemon.name }}
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import { fetchPokemonList } from '../../services/pokeapi';
+
+export default {
+    name: "HomeView",
+    data() {
+        return {
+            pokemonList: [],
+        };
+    },
+
+    async mounted() {
+        const data = await fetchPokemonList(0, 100);
+        if (data && data.results) {
+            this.pokemonList = data.results;
+        }
+    }
+
+
+};
+
+</script>
+
+<style src="./homeview.css"></style>
